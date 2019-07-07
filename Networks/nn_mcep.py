006e443eddb86700c2dbbdd367e7279767407fc9
yamachu <yamachu.dev@gmail.com>
2019-07-07 16:07:42 +0900

    Optimizerのところだめだー

--- Networks/nn_mcep.py
+++ Networks/nn_mcep.py
@@ -1,34 +1,41 @@
 import tensorflow as tf
 
-# Constant
-input_size = 177 # Passed feature size
-output_size = 177 # Number of outputs
-hidden_size = 500
-
-# Todo: Batch input...
 
 tf.set_random_seed(1)
 
-X = tf.placeholder(tf.float32, [None, input_size], name="X")
-Y = tf.placeholder(tf.float32, [None, output_size], name="Y")
-lr = tf.placeholder(tf.float32, name = "learning_rate")
+# Constant
+input_size = 40 # Passed feature size
+output_size = 40 # Number of outputs
+hidden_size = 500
 
-h1 = tf.layers.dense(inputs=X,units=hidden_size,activation=tf.nn.leaky_relu)
-h2 = tf.layers.dense(inputs=h1,units=hidden_size,activation=tf.nn.leaky_relu)
-h3 = tf.layers.dense(inputs=h2,units=hidden_size,activation=tf.nn.leaky_relu)
+# Parameters
+lr = tf.placeholder(tf.float32, name='learning_rate')
 
-W = tf.Variable(tf.random_normal([hidden_size,output_size], stddev=0.1), name = 'W4')
-b = tf.Variable(tf.zeros([output_size]), name = 'b4')
+# Todo: Batch input...
 
-Converted = tf.add(tf.add(tf.matmul(h3, W), b), X, name = 'Converted')
+# Graph Input
+X = tf.placeholder(tf.float32, [None, input_size], name='X')
+# Graph Label
+Y = tf.placeholder(tf.float32, [None, output_size], name='Y')
 
-diff = tf.subtract(Converted, Y)
-loss = tf.nn.l2_loss(diff)
-loss = tf.identity(loss, name="Loss")
-optimizer = tf.train.AdamOptimizer(learning_rate=lr, name='Optimizer').minimize(loss)
+h1 = tf.layers.dense(inputs=X, units=hidden_size, activation=tf.nn.leaky_relu)
+h2 = tf.layers.dense(inputs=h1, units=hidden_size, activation=tf.nn.leaky_relu)
+h3 = tf.layers.dense(inputs=h2, units=hidden_size, activation=tf.nn.leaky_relu)
+
+W = tf.Variable(tf.random_normal([hidden_size, output_size], stddev=0.1), name='W4')
+b = tf.Variable(tf.zeros([output_size]), name='b4')
+Converted = tf.add(tf.add(tf.matmul(h3, W), b), X, name='Converted')
+
+loss = tf.nn.l2_loss(tf.subtract(Converted, Y), name='Loss')
+optimizer = tf.train.AdamOptimizer(learning_rate=lr).minimize(loss, name='Optimizer')
 
 init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
     sess.run(init)
-    tf.saved_model.simple_save(sess, r'../Models/McepNN/model', inputs={'X': X, 'Y': Y}, outputs={'Converted': Converted} )
+    tf.saved_model.simple_save(
+        sess,
+        '../Models/McepNN/model',
+        inputs={'X': X, 'Y': Y},
+        outputs={'Converted': Converted}
+    )
